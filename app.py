93f9d7cd4d1c99a67ebcd7f40b438de6d166ceb1
Shubham Awasthi <shubham0522@gmail.com>
2018-06-16 00:45:58 +0530

    App page

---
+++ app.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.insert(0, './routes')
+from flask import Flask, jsonify, Response, request
+import members
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = './tmp/'
+
+@app.route('/',methods=['post','get'])
+def index():
+    return '<form action="/members" method="post" enctype="multipart/form-data"><input type="file" name="file" /><input type="submit"></form>'
+
+members.routes(app)
+
+# @app.errorhandler(400)
+# def err400(err):
+#     print(err)
+#     return (jsonify({'err': 'bad request'}), 400)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
