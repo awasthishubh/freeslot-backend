d5a451d30426b8e563deb2f9bba9468515d70dd7
Shubham Awasthi <shubham0522@gmail.com>
2018-06-18 13:00:29 +0530

    Finds Free slot timmings from free slots

--- routes/members.py
+++ routes/members.py
@@ -6,6 +6,8 @@ import members_model
 def routes(app,db):
     @app.route('/members',methods=['post'])
     def memindex():
+        if(members_model.exists(db,request.form['reg'],request.form['org'])):
+            return(jsonify({'status':409, 'err':'User Already registered under the organisation'}),409)
         name=''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
         data=request.form
         f = request.files['timeTable']
@@ -13,21 +15,16 @@ def routes(app,db):
         slots=preproc('./tmp/'+name+'.png')
         os.remove('./tmp/'+name+'.png')
         if(slots):
-            freeSlots=[]
-            for i in range(len(slots)):
-                if(not slots[i]):
-                    freeSlots.append(i)
-
             details={
                 'name':data['name'],
                 'reg':data['reg'],
                 'org':data['org'],
                 'email':data['email'],
                 'phno':data['phno'],
-                'slots':freeSlots
+                'slots':slots
             }
             stat=members_model.insert(db,details)
-            if stat==409:
-                return(jsonify({'status':409, 'err':'User Already Exists'}),409)
+            if stat==500:
+                return(jsonify({'status':500, 'err':'Internal Server Error'}),500)
             return(jsonify({'status':200, 'data':details}),200)
         return (jsonify({'err':'bad file'}),400)
