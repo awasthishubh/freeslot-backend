021b84611365170051fc0a8b68c07fcccd5c8735
awasthishubh <aw.shubh@gmail.com>
2018-07-24 00:26:14 +0530

    Added org detail update api

--- models/org_model.py
+++ models/org_model.py
@@ -13,15 +13,32 @@ class Organisations():
         else:
             self.db.organisations.insert_one(data)
             data['_id']=str(data['_id'])
+            if('passwd' in data.keys()): del data['passwd']
             return (data,200)
 
     def auth(self,data):
         user=self.db.organisations.find_one({'usid':data['usid']})
         if(user and user['passwd']==data['passwd']):
             user['_id']=str(user['_id'])
+            if('passwd' in data.keys()): del data['passwd']
             return (user,200)
         else:
             return (None,401)
+
+    def update(self, data):
+        user=self.db.organisations.update_one({
+            'usid':data['usid'],
+            'passwd':data['passwd']
+        },{
+            '$set': {
+                'descr': data['descr'],
+                'dp': data['dp'],
+                'name': data['name'],
+                'passwd': data['newPasswd']
+            }
+        }, upsert=False)
+
+        return user.matched_count
 
     def exists(self,usid):
         sts=self.db.organisations.find_one({'usid':usid})
@@ -37,5 +54,6 @@ class Organisations():
 
     def org(self, usid):
         data=self.db.organisations.find_one({'usid':usid})
+        if('passwd' in data.keys()): del data['passwd']
         data['_id']=str(data['_id'])
         return data
