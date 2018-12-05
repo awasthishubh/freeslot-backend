a68b73ccdebfaa88c4972975c1b4c360452f1720
Shubham Awasthi <aw.shubh@gmail.com>
2018-12-05 19:37:09 +0530

    Updated db

--- models/model.py
+++ models/model.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from keys import keys
 
-db = MongoClient(keys.db).freeslots
+db = MongoClient(keys.db).freeslot_2
 
 from members_model import Members
 from org_model import Organisations
